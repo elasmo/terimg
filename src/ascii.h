088b468821589bee273cba856ef9ae45beb2b5d8
elasmo <johan@jails.se>
2016-01-03 21:05:39 +0100

    Initial commit

---
+++ src/ascii.h
@@ -0,0 +1,16 @@
+/*                                                                             
+* ----------------------------------------------------------------------------  
+* "THE BEER-WARE LICENSE" (Revision 42):                                        
+* <Johan> wrote this file. As long as you retain this notice you
+* can do whatever you want with this stuff. If we meet some day, and you think  
+* this stuff is worth it, you can buy me a beer in return. Johan                
+* ----------------------------------------------------------------------------  
+*/                   
+
+#ifndef ASCII_H
+#define ASCII_H
+
+#define SPACE   32
+#define ESCAPE  27
+#define ENTER   10
+#endif
